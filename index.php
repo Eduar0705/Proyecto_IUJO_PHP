0bd2a1fd8f9a1a4dd61cf110087df2caa10e7469
eduar suarez <eduarjosesuarezgraterol@gmail.com>
2025-05-03 07:04:23 -0400

    v1

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>INVILARA</title>
+    <link rel="stylesheet" href="./styles/style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="container">
+                <ul>
+                    <li><a href="./index.php">Inicio</a></li>
+                    <li><a href="./invetario_Compras.php">Invetario de Compras</a></li>
+                    <li><a href="./verificacion.php">Tramites y procesos</a></li>
+                    <li><a href="./justificaciones.php">Aprovaciones</a></li>
+                    <li><a href="./Solicitudes.php">Solicitudes</a></li>
+                    <li><a href="./materiales.php">Solicitud de Materiales</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+</body>
+</html>
